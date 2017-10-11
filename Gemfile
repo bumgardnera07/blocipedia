55a7259332f049d218d85fc7c247f912b5cba093
George Bumgardner <bumgardnera07@gmail.com>
2017-10-11 02:25:02 +0000

    roles implemented

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@
  gem 'bootstrap-sass'
  gem 'figaro'
  gem 'devise'
+ gem 'pundit'
  
  group :development, :test do
    gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :wikis
+  resources :users, only: [:update]
 
   devise_for :users
   
   get 'about' => 'welcome#about'
   
-  root 'welcome#index'
+  root 'wikis#index'
 end
